5c68cea4d853d4e4d50c70924bf7a7815b47f011
EvilAlliance <chialanza17@gmail.com>
2023-08-18 09:24:23 -0300

    Separacion de Contantes

--- src/Constantes/Index.ts
+++ src/Constantes/Index.ts
@@ -2,6 +2,7 @@ export const FirstLayerAttribute = '[layer="1"]';
 
 export const ShowMenuClass = 'open';
 
+export const ColorPickerBId = '#ColorPicker';
 export const ColorPickerAClass = '.pickr';
 export const ColorPickerTheme = 'nano';
 export const ColorPickerPosition = 'right-middle';
--- src/Constantes/JSPath.ts
+++ src/Constantes/JSPath.ts
@@ -1,9 +1,8 @@
-export const PencilButtonId = '#Pencil'
-export const PencilMenuId = '#PencilMenu'
-export const ColorPickerBId = '#ColorPicker'
-export const TiltleBarId = '#AcctionBotton'
-export const FabricUpperCanvasClass = '.upper-canvas'
-export const PencilMenuSampleColor = `${PencilMenuId} > .colors > .circle`
-export const PencilMenuPeview = `${PencilMenuId} > .circle`
-export const PencilMenuSize = `${PencilMenuId} > .size > input[type="range"]`
+export const PencilButtonId = '#Pencil';
+export const PencilMenuId = '#PencilMenu';
+export const TiltleBarId = '#AcctionBotton';
+export const FabricUpperCanvasClass = '.upper-canvas';
+export const PencilMenuSampleColor = `${PencilMenuId} > .colors > .circle`;
+export const PencilMenuPeview = `${PencilMenuId} > .circle`;
+export const PencilMenuSize = `${PencilMenuId} > .size > input[type="range"]`;
 
